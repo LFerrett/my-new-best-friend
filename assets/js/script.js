1b3dacc6feb568b8065f7cf562b68959fb506c50
robertsjn2000 <robertsjn2000@gmail.com>
2021-12-21 15:13:01 -0500

    initial script for API call function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
 var petFinderApiKey = "6MI42Rvs7RjJGQQTiwMErXRHg5hdT08iBt1zKyzysWnTt7BPzx"
 var theDogApiKey = "9af1f589-f293-4bba-8d9e-3ba9732efb0f"
+var breedTextBox = document.getElementById("breed")
+function getBreedInfo(){
+    var Url = "https://api.thedogapi.com/v1/breeds?attach_breed=0" + theDogApiKey
+        fetch(Url).then(function(breed){
+            console.log(breed);
+        });
+
+        
+}    
