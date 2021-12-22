6e5e556bdf73c196efb339c12a46cdbfc524743e
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-22 11:00:05 -0500

    Added global blank variables for zip code and distance from location for searches, global variables to be linked to search boxes when id's are finalized, and added to petfinder api fetch request for sorting only for dogs and searching by zip code and distance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 var petFinderApiKey = "6MI42Rvs7RjJGQQTiwMErXRHg5hdT08iBt1zKyzysWnTt7BPzx"
 var petFinderSecret = "qQWrLT0bSjdrEfmGEXO7xuBTx9SY5KdHCQ4v4GOf"
 var theDogApiKey = "9af1f589-f293-4bba-8d9e-3ba9732efb0f"
+var zipCode;
+var distance;
 
 // Function to call the Petfinder API
 function callPetFinder() {
+
+  // To be defined when function is called with input fields on html
+  // zipCode = ;
+  // distance = ;
 
   // Fetch code referenced from https://gomakethings.com/using-oauth-with-fetch-in-vanilla-js/
   // Fetch to retrieve the Petfinder authorization tokens
@@ -21,7 +27,7 @@ function callPetFinder() {
   }).then(function (tokenData) {
 
     // Uses the returned authorization tokens to fetch Petfinder's API
-    return fetch('https://api.petfinder.com/v2/animals', {
+    return fetch('https://api.petfinder.com/v2/animals?type=dog&location=' + zipCode + '&distance=' + distance, {
       headers: {
         'Authorization': tokenData.token_type + ' ' + tokenData.access_token,
         'Content-Type': 'application/x-www-form-urlencoded'
