2439e587682c01b505e391f8b488d9875c2aa114
robertsjn2000 <robertsjn2000@gmail.com>
2021-12-22 11:47:08 -0500

    changes to the fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,20 @@ var petFinderApiKey = "6MI42Rvs7RjJGQQTiwMErXRHg5hdT08iBt1zKyzysWnTt7BPzx"
 var theDogApiKey = "9af1f589-f293-4bba-8d9e-3ba9732efb0f"
 var breedTextBox = document.getElementById("breed")
 function getBreedInfo(){
-    var Url = "https://api.thedogapi.com/v1/breeds" + theDogApiKey
-        fetch(Url).then(function(data){
-            console.log(response.data);
+    var Url = "https://api.thedogapi.com/v1/images/search"
+        fetch(Url, {
+           headers:{
+               "x-api-key": theDogApiKey
+           } 
+        }).then(function(response){
+            // console.log(response.json());
+            return response.json()
+        
+        }).then(function(data){
+            console.log(data)
         });
+    
 
         
 }    
+getBreedInfo()
